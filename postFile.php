18e10581ec04895d3b7d13ad3af2bb153f60deed
Catalin Sava <72122730+catalynsava@users.noreply.github.com>
2021-08-06 16:27:11 +0300

    up 06.08.21

---
+++ postFile.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>postFile</title>
+</head>
+<body>
+    <?php
+        if($_POST["data"] == "100"){
+            echo("100");
+        }
+    ?>
+</body>
+</html>
